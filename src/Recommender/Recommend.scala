42ae60bebc27c63f21605bff299e9a496604d0a3
Tai-Yi Kuo <b96705008@gmail.com>
2016-04-12 22:10:17 +0800

    first commit

---
+++ src/Recommender/Recommend.scala
@@ -0,0 +1,104 @@
+package Recommender
+
+import java.io.File
+import scala.io.Source
+import org.apache.log4j.{Logger, Level}
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.SparkContext._
+import org.apache.spark.rdd._
+import org.apache.spark.mllib.recommendation.{ALS, Rating, MatrixFactorizationModel}
+import scala.collection.immutable.Map
+
+/**
+  * Created by roger19890107 on 4/4/16.
+  */
+object Recommend {
+  def main(args: Array[String]) {
+    //Log only warn
+    setLogger
+
+    //Prepare data
+    println("prepare data...")
+    val (ratings, movieTitle) = prepareData()
+
+    //Train model
+    println("start train model using data count: " + ratings.count())
+    val model = ALS.train(ratings, 5, 20, 0.1)
+    println("finish training model...")
+
+    //Recommend phase
+    println("start recommend...")
+    recommend(model, movieTitle)
+  }
+
+  def setLogger: Unit = {
+    Logger.getLogger("org").setLevel(Level.OFF)
+    Logger.getLogger("com").setLevel(Level.OFF)
+    System.setProperty("spark.ui.showConsoleProgress", "false")
+    Logger.getRootLogger.setLevel(Level.OFF)
+  }
+
+  def prepareData(): (RDD[Rating], Map[Int, String]) = {
+    // 1. build user rating data
+    val sc = new SparkContext(new SparkConf()
+      .setAppName("Recommend").setMaster("local[*]"))
+    sc.setCheckpointDir("checkpoint/")
+    val rawUserData = sc.textFile("data/ml-100k/u.data")
+    val rawRatings = rawUserData.map(_.split("\t").take(3))
+    val ratingsRDD = rawRatings.map {
+      case Array(user, movie, rating) => Rating(user.toInt, movie.toInt, rating.toDouble)
+    }
+    println("total: " + ratingsRDD.count().toString + "ratings.")
+
+    // 2. build movie id map to name
+    println("load the items data...")
+    val itemRDD = sc.textFile("data/ml-100k/u.item")
+    val movieTitle = itemRDD.map(line => line.split("\\|").take(2))
+      .map(array => (array(0).toInt, array(1))).collect().toMap
+
+    // 3. show data
+    val numRatings = ratingsRDD.count()
+    val numUsers = ratingsRDD.map(_.user).distinct().count()
+    val numMovies = ratingsRDD.map(_.product).distinct().count()
+    println("total ratings: " + numRatings + " User " + numUsers + " Movie " + numMovies)
+    return (ratingsRDD, movieTitle)
+  }
+
+  def recommendMovies(model: MatrixFactorizationModel, movieTitle: Map[Int, String], inputUserID: Int) = {
+    val recommendMovie = model.recommendProducts(inputUserID, 10)
+    println("For user: " + inputUserID + ", we recommend the movies:")
+    var i = 0
+    recommendMovie.foreach(r => {
+      println(i.toString + "." + movieTitle(r.product) + " ratings: " + r.rating.toString)
+      i += 1
+    })
+  }
+
+  def recommendUsers(model: MatrixFactorizationModel, movieTitle: Map[Int, String], inputMovieID: Int) = {
+    val recommendUser = model.recommendUsers(inputMovieID, 10)
+    println("For movie: " + inputMovieID + " name: "+ movieTitle(inputMovieID) + ", we recommend the users:")
+    var i = 0
+    recommendUser.foreach(r => {
+      println(i.toString + " user id: " + r.user + " ratings: " + r.rating.toString)
+      i += 1
+    })
+  }
+
+  def recommend(model: MatrixFactorizationModel, movieTitle: Map[Int, String]) = {
+    var choose = ""
+    while (choose != "3") {
+      print("recommend type? 1.movies for user 2.users for movie 3.exit: ")
+      choose = readLine()
+
+      if (choose == "1") {
+        print("user id?")
+        val inputUserID = readLine()
+        recommendMovies(model, movieTitle, inputUserID.toInt)
+      } else if (choose == "2") {
+        print("movie id?")
+        val inputMovieID = readLine()
+        recommendUsers(model, movieTitle, inputMovieID.toInt)
+      }
+    }
+  }
+}
---
+++ src/Utils/Chart.scala
@@ -0,0 +1,52 @@
+package Utils
+
+import java.awt.Color
+
+import org.jfree.chart.axis.{AxisLocation, NumberAxis}
+import org.jfree.chart.labels.StandardCategoryToolTipGenerator
+import org.jfree.chart.plot.{DatasetRenderingOrder, PlotOrientation}
+import org.jfree.chart.renderer.category.LineAndShapeRenderer
+import org.jfree.chart.{ChartFactory, ChartFrame}
+import org.jfree.data.category.DefaultCategoryDataset
+
+/**
+  * Created by roger19890107 on 4/5/16.
+  */
+object Chart {
+  def plotBarLineChart(title: String, xLabel: String, yBarLabel: String,
+                       yBarMin: Double, yBarMax: Double, yLineLabel: String,
+                       dataBarChart: DefaultCategoryDataset,
+                       dataLineChart: DefaultCategoryDataset) = {
+
+    // build bar chart
+    val chart = ChartFactory.createBarChart(
+      "", xLabel, yBarLabel, dataBarChart,
+      PlotOrientation.VERTICAL, true, true, false)
+
+    // get plot
+    val plot = chart.getCategoryPlot
+    plot.setBackgroundPaint(new Color(0xEE, 0xEE, 0xFF))
+    plot.setDomainAxisLocation(AxisLocation.BOTTOM_OR_RIGHT)
+    plot.setDataset(1, dataLineChart)
+
+    // bar chart y axis
+    val vn = plot.getRangeAxis()
+    vn.setRange(yBarMin, yBarMax)
+
+    // line chart y axis
+    val axis2 = new NumberAxis(yLineLabel)
+    plot.setRangeAxis(1, axis2)
+
+    // set line chart in front of bar chart
+    val renderer2 = new LineAndShapeRenderer()
+    renderer2.setToolTipGenerator(new StandardCategoryToolTipGenerator())
+    plot.setRenderer(1, renderer2)
+    plot.setDatasetRenderingOrder(DatasetRenderingOrder.FORWARD)
+
+    // build frame
+    val frame = new ChartFrame(title, chart)
+    frame.setSize(500, 500)
+    frame.pack()
+    frame.setVisible(true)
+  }
+}
