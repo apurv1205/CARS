d26727399e860211a47f72a309a7a3ee6db3818b
Roger Kuo <b96705008@gmail.com>
2016-05-30 17:24:57 +0800

    refactor

--- src/Recommender/KNNEvaluate.scala
+++ src/Recommender/KNNEvaluate.scala
@@ -1,8 +1,7 @@
 package Recommender
 
+import Context.Context
 import org.apache.spark.mllib.linalg.distributed.{CoordinateMatrix, MatrixEntry}
-import org.apache.spark.mllib.recommendation.Rating
-import org.apache.spark.rdd.RDD
 import org.apache.spark.mllib.rdd.MLPairRDDFunctions._
 
 
@@ -10,33 +9,36 @@ import org.apache.spark.mllib.rdd.MLPairRDDFunctions._
   * Created by roger19890107 on 5/21/16.
   */
 object KNNEvaluate extends App {
-  Utils.Config.setLogger
-  val sc = Utils.Config.setupContext("KNN Evaluate")
-  val K = 25
+  Context.setLogger
+  val (sc, _) = Context.initSpark("KNN Evaluate")
+  val K = 2
+  val uIndex = 0
+  val pIndex = 1
 
   //==== init data ====
   println("load installations data ...")
-  val rawDataWithHeader = sc.textFile("data/installations.csv")
+  //val rawDataWithHeader = sc.textFile("data/installations.csv")
+  val rawDataWithHeader = sc.textFile("data/behavior.csv")
   val rawData = rawDataWithHeader.mapPartitionsWithIndex {
     (idx, iter) => if (idx == 0) iter.drop(1) else iter
   }.map(_.split(","))
 
   // product map
-  val pNameToIndex = rawData.map(_(0)).distinct.zipWithIndex().collectAsMap()
+  val pNameToIndex = rawData.map(_(pIndex)).distinct.zipWithIndex().collectAsMap()
   val pIndexToName = Map() ++ pNameToIndex.map(_.swap)
   val pNameToIndexVar = sc.broadcast(pNameToIndex)
 
   // user map
-  val uNameToIndex = rawData.map(_(1)).distinct.zipWithIndex().collectAsMap()
+  val uNameToIndex = rawData.map(_(uIndex)).distinct.zipWithIndex().collectAsMap()
   val uIndexToName = Map() ++ uNameToIndex.map(_.swap)
   val uNameToIndexVar = sc.broadcast(uNameToIndex)
 
   //==== build matrix ====
   val entries = rawData.map { line =>
-      val product = pNameToIndexVar.value(line(0))
-      val user = uNameToIndexVar.value(line(1))
-      val rating = line(2).toDouble
-      MatrixEntry(user, product, rating)
+    val product = pNameToIndexVar.value(line(pIndex))
+    val user = uNameToIndexVar.value(line(uIndex))
+    val rating = line(2).toDouble
+    MatrixEntry(user, product, rating)
   }.filter(_.value != 0)
 
   // R: rating matrix
@@ -46,12 +48,16 @@ object KNNEvaluate extends App {
   println("Rating Matrix: " + userNum + "*" + itemNum)
 
   // S: item similarity matrix
-  val itemSimMat = ratingMat.toIndexedRowMatrix().columnSimilarities()
-  println("Item Similarity Matrix: " + itemSimMat.numRows() + "*" + itemSimMat.numCols())
+  println("Calculate item similarities ...")
+  val partialSimMat = ratingMat.toIndexedRowMatrix().columnSimilarities()
+  val itemSimEntries = partialSimMat.entries.flatMap {
+    case m @ MatrixEntry(i, j, value) if i != j =>
+      List(m, MatrixEntry(j, i, value))
+  }
 
   // SK: top k similar item matrix
   println("Extract top K entries ...")
-  val topKItemEntries = itemSimMat.entries
+  val topKItemEntries = itemSimEntries
     .filter(m => m.i != m.j)
     .map(m => (m.i, m))
     .topByKey(K)(Ordering.by(_.value))
@@ -60,11 +66,10 @@ object KNNEvaluate extends App {
   println("top K item matrix: " + topKItemMat.numRows() + "*" + topKItemMat.numCols())
 
   // SS: Sum of top K similarity matrix
+  println("Sum Similarity matrix ...")
   val sumSimEntries = topKItemEntries.map(m => (m.i, m.value)).reduceByKey(_ + _).map {
     case (i, sumValue) => MatrixEntry(i, 0, sumValue)
   }
-  val sumSimMat = new CoordinateMatrix(sumSimEntries, itemNum, 1)
-  println("Sum Similarity matrix: " + sumSimMat.numRows() + "*" + sumSimMat.numCols())
 
   // P: predict matrix
   println("Start predicting the item-based recommender ...")
@@ -78,14 +83,13 @@ object KNNEvaluate extends App {
     .mapValues {
       case (m, sumVal) =>
         val normVal = if (sumVal == 0) 0 else m.value / sumVal
+        //val normVal = m.value
         MatrixEntry(m.i, m.j, normVal)
     }.map(_._2)
-//  val normalPredictMat = new CoordinateMatrix(normPredictEntries, userNum, itemNum)
-//  println("normal predict matrix: " + normalPredictMat.numRows() + "*" + normalPredictMat.numCols())
 
   println("recommend for User ...")
-  val topN = 10
-  val userName = "1"
+  val topN = 5
+  val userName = "u1"
   val userIndex = uNameToIndex(userName)
 
   normPredictEntries
@@ -93,10 +97,6 @@ object KNNEvaluate extends App {
     .map(m => (m.j, m.value))
     .top(topN)(Ordering.by(_._2))
     .foreach {
-      case (index, value) => println(pIndexToName(index) + "" + value)
+      case (index, value) => println(pIndexToName(index) + " - " + value)
     }
-
-//    .map(_._1).foreach {
-//      index => println(pIndexToName(index) + "")
-//    }
 }
